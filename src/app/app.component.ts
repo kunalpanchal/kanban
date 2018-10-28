5f5f95e17b7b031dc00a7293f6aa3582363e2570
Kay <hello@kunalpanchal.in>
2018-10-28 12:54:29 +0530

    [Basic design] : Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+declare var jQuery: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'kanban';
+  statuses: ['pending', 'done', 'To-do', 'In Progress'];
+  modal: any = {
+    hidden: true
+  }
+  boards: any = [{
+    name: 'Pitching',
+    color: '#ffcccb',
+    projects: [{
+      name: 'Game of Thrones',
+      description: 'jkhfjkhsadf',
+      status: 'pending'
+    }, {
+      name: 'Game of Thrones',
+      description: 'jkhfjkhsadf',
+      status: 'pending'
+    }, {
+      name: 'Game of Thrones',
+      description: 'jkhfjkhsadf',
+      status: 'pending'
+    }, {
+      name: 'Game of Thrones',
+      description: 'jkhfjkhsadf',
+      status: 'pending'
+    }, {
+      name: 'Game of Thrones',
+      description: 'jkhfjkhsadf',
+      status: 'pending'
+    }, {
+      name: 'Game of Thrones',
+      description: 'jkhfjkhsadf',
+      status: 'pending'
+    }]
+  },
+  {
+    name: 'OnGoing',
+    color: '#ffc4ff',
+    projects: []
+  },
+  {
+    name: 'Post Production',
+    color: '#c3fdff',
+    projects: []
+  },
+  {
+    name: 'Complete',
+    color: '#d7ffd9',
+    projects: []
+  }, {
+    name: 'Testing',
+    color: '#b4ffff',
+    projects: []
+  }]
+  ngOnInit() {
+
+    // jQuery("mat-card-board, div").disableSelection();
+    // jQuery(".mat-card-board").draggable({
+    //   // appendTo: 'body',
+    //   zIndex: 99999999,
+    //   connectToSortable: ".mat-card-board",
+    //   containment: 'document',
+    //   scroll: false,
+    //   // revert: true,
+    //   helper: "clone"
+    // });
+  }
+
+  ngAfterViewInit() {
+    this.initDrag()
+  }
+  initDrag() {
+    jQuery(".droppable").sortable({
+      connectWith: ".droppable",
+      /*stop: function(event, ui) {
+          var item_sortable_list_id = $(this).attr('id');
+          console.log(ui);
+          //alert($(ui.sender).attr('id'))
+      },*/
+      receive: function (event, ui) {
+
+      }
+    }).disableSelection();;
+
+    jQuery(".project-card").draggable({
+      // handle: "i",
+      cancel: "div.testtt",
+      connectToSortable: '.droppable',
+      // helper: 'clone'
+      scroll: false,
+      revert: true,
+      zIndex: 99999999,
+
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
